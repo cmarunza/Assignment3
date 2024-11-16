3bdb26fb7d7a3e59bb7e1c8d7e07008b4c28a104
cmarunza <167131881+cmarunza@users.noreply.github.com>
2024-11-16 14:30:45 -0600

    Update RedBlackTreeNode.java

--- RedBlackTreeNode.java
+++ RedBlackTreeNode.java
@@ -1,1 +1,11 @@
+public class RedBlackTreeNode {
+    Product product;
+    RedBlackTreeNode left, right, parent;
+    boolean color; // true for red, false for black
+
+    public RedBlackTreeNode(Product product) {
+        this.product = product;
+        this.color = true; // new nodes are red by default
+    }
+}
 
