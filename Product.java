0b4823a705fc70793fd617867d9c761d10ad9afa
cmarunza <167131881+cmarunza@users.noreply.github.com>
2024-12-20 10:47:06 -0600

    Update Product.java

--- Product.java
+++ Product.java
@@ -1,1 +1,18 @@
+public class Product {
+    String productId;
+    String name;
+    String category;
+    String price;
 
+    public Product(String productId, String name, String category, String price) {
+        this.productId = productId;
+        this.name = name;
+        this.category = category;
+        this.price = price;
+    }
+
+    @Override
+    public String toString() {
+        return "Product ID: " + productId + ", Name: " + name + ", Category: " + category + ", Price: " + price;
+    }
+}
